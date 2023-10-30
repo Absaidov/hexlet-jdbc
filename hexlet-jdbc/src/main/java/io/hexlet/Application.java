6b7914f3c91cff47136f1a45e299a9e4b09bbd1f
Absaidov <dabsaidov@bk.ru>
2023-10-30 17:21:37 +0300

    4 lesson jdbc course. didnt finished

--- hexlet-jdbc/src/main/java/io/hexlet/Application.java
+++ hexlet-jdbc/src/main/java/io/hexlet/Application.java
@@ -1,31 +1,31 @@
 package io.hexlet;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import io.hexlet.User;
+
 
 public class Application {
-
     public static void main(String[] args) throws SQLException {
+        // BEGIN (write your solution here)
+        try (var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet")) {
 
-        try (var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test")) {
-            var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+            var sql = "CREATE TABLE users "
+                    + "(id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
 
             try (var statement = conn.createStatement()) {
                 statement.execute(sql);
             }
-                var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-            try (var statement2 = conn.createStatement()) {
-                statement2.executeUpdate(sql2);
-            }
-                var sql3 = "SELECT * FROM users";
-            try (var statement3 = conn.createStatement()) {
-                var resultSet = statement3.executeQuery(sql3);
-                while (resultSet.next()) {
-                    System.out.println(resultSet.getString("username"));
-                    System.out.println(resultSet.getString("phone"));
-                }
-            }
+            var dao = new UserDAO(conn);
+            var user = new User("jamal", "90999");
+            System.out.println(user.getId());
+            dao.save(user);
+//            System.out.println(dao.find(1L));
         }
-
+//        var conn =  DriverManager.getConnection("jdbc:h2:mem:hexlet");
+//            System.out.println(user.getName());
     }
 }
